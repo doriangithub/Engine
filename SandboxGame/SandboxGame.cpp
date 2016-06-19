a422cf26b16fc559a96a96fdcea7850c9d09d20b
Dorian Sharevich <dorian.sharevich@gmail.com>
2016-06-19 15:02:16 -0700

    Add MyGLWindow class

--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -1,8 +1,12 @@
 #include <QtWidgets\qapplication.h>
+#include <QtWidgets\qwidget.h>
+#include "MyGLWindow.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc,argv);
+	MyGLWindow myGlWindow;
+	myGlWindow.show();
 
 	return app.exec();
 }
