9357fabf3e6f7d1927aa46d446abc39eed674225
Dorian Sharevich <dorian.sharevich@gmail.com>
2016-06-21 09:18:47 -0700

    Moved the triangle

--- SandboxGame/MyGLWindow.cpp
+++ SandboxGame/MyGLWindow.cpp
@@ -1,8 +1,17 @@
 #include <GL\glew.h>
 #include "MyGLWindow.h"
 #include <cassert>
-//#include <iostream>
+#include <Math\Vector2D.h>
+using Math::Vector2D;
 
+static Vector2D verts[] =
+{
+	Vector2D(+0.0f, +0.1f),
+	Vector2D(-0.1f, -0.1f),
+	Vector2D(+0.1f, -0.1f),
+};
+
+static const unsigned int NUM_VERTS = sizeof(verts) / sizeof(*verts);
 
 void MyGLWindow::initializeGL()
 {
@@ -12,14 +21,7 @@ void MyGLWindow::initializeGL()
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 
-	float verts[] =
-	{
-		+0.0f, +0.1f,
-		-0.1f, -0.1f,
-		+0.1f, -0.1f,
-	};
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), NULL, GL_DYNAMIC_DRAW);
 
 	connect(&myTimer, SIGNAL(timeout()), this, SLOT(myUpdate()));
 	myTimer.start(0);
@@ -31,10 +33,20 @@ void MyGLWindow::paintGL()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	Vector2D shipPosition(0.5f, 0.5f);
+
+	Vector2D translatedVerts[NUM_VERTS];
+	for (unsigned int i = 0; i < NUM_VERTS; i++)
+	{
+		translatedVerts[i] = verts[i] + shipPosition;
+	}
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(translatedVerts), translatedVerts);
+
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void MyGLWindow::myUpdate()
 {
-	//std::cout << "Frame" << std::endl;
+
 }
