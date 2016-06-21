ad92cb0cd7005ef4270de9efd000e2727e22155d
Dorian Sharevich <dorian.sharevich@gmail.com>
2016-06-21 07:28:34 -0700

    Added Vector2D and Vector2D tests

---
+++ EngineTester/Vector2DTests.cpp
@@ -0,0 +1,34 @@
+#include <gtest\gtest.h>
+#include "Vector2D.h"
+using Math::Vector2D;
+
+TEST(Vector2D, Constructor)
+{
+	Vector2D first(1.0, 2.0);
+	EXPECT_FLOAT_EQ(1, first.x);
+	EXPECT_FLOAT_EQ(2, first.y);
+}
+
+TEST(Vector2D, VectorAddition)
+{
+	Vector2D first(1.0, 2.0);
+	Vector2D second(3.0, 4.0);
+	Vector2D result1 = first + second;
+	Vector2D result2 = second + first;
+	EXPECT_FLOAT_EQ(4, result1.x);
+	EXPECT_FLOAT_EQ(6, result1.y);
+
+	EXPECT_FLOAT_EQ(result1.x , result2.x);
+	EXPECT_FLOAT_EQ(result1.y , result2.y);
+}
+
+TEST(Vector2D, ScalarMultiplication)
+{
+	Vector2D vec(-7, 3);
+	Vector2D result1 = 8 * vec;
+	Vector2D result2 = vec * 8;
+	EXPECT_FLOAT_EQ(result1.x, -56);
+	EXPECT_FLOAT_EQ(result1.y, 24);
+	EXPECT_FLOAT_EQ(result1.x, result2.x);
+	EXPECT_FLOAT_EQ(result1.y, result2.y);
+}
