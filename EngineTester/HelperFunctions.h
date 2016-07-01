0ca624aac9c85db89e07db4f0b725997eb5b7339
Dorian Sharevich <dorian.sharevich@gmail.com>
2016-07-01 12:51:56 -0700

    Matrix2D rotation testing

---
+++ EngineTester/HelperFunctions.h
@@ -0,0 +1,10 @@
+#ifndef TESTER_HELPER_FUNCTIONS_H
+#define TESTER_HELPER_FUNCTIONS_H
+#include <cmath>
+
+inline bool closeEnough(float left, float right, float treshold = 0.0000001) 
+{
+	return abs(left - right) < treshold;
+}
+
+#endif // !TESTER_HELPER_FUNCTIONS_H
