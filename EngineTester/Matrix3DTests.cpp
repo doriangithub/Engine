6959f92209805b1b39e82e23b3e442ed6902c6c3
Dorian Sharevich <dorian.sharevich@gmail.com>
2016-07-03 18:00:34 -0700

    Testing and Implementing Matrix3D Multiplition to Vector3D

--- EngineTester/Matrix3DTests.cpp
+++ EngineTester/Matrix3DTests.cpp
@@ -1,6 +1,8 @@
 #include <gtest\gtest.h>
 #include <Math\Matrix3D.h>
+#include <Math\Vector3D.h>
 using Math::Matrix3D;
+using Math::Vector3D;
 
 TEST(Matrix3D, Conctractor)
 {
@@ -33,5 +35,15 @@ TEST(Matrix3D, Conctractor)
 
 TEST(Matrix3D, MatrixVectorMultiply)
 {
+	Matrix3D op(
+		1.0f, 2.0f, 3.0f,
+		4.0f, 5.0f, 6.0f,
+		7.0f, 8.0f, 9.0f);
 
+	Vector3D victim(1.0f, 2.0f, 3.0f);
+
+	Vector3D victimPrime = op * victim;
+	EXPECT_FLOAT_EQ(victimPrime.x, 14.0f);
+	EXPECT_FLOAT_EQ(victimPrime.y, 32.0f);
+	EXPECT_FLOAT_EQ(victimPrime.z, 50.0f);
 }
